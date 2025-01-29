59f4e7c8b5f20316e3743793faa69211150bc7b9
srbutler1 <srb019@uark.edu>
2025-01-29 12:40:27 -0600

    edited requirements.txt

--- data_analysis_streamlit_app.py
+++ data_analysis_streamlit_app.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 os.environ["STREAMLIT_SERVER_MAX_UPLOAD_SIZE"] = "2000"
-os.environ["OPENAI_API_KEY"] = "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"
 import streamlit as st
 
 # Set Streamlit to wide mode
